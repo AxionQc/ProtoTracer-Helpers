972d24759e192e6335574fb3ce2e4b4b89c97a14
Noah <119726313+AxionQc@users.noreply.github.com>
2025-06-29 21:49:53 -0500

    Update ImageConverter.py to Seperate Image data to a CPP

--- Image-Converter/ImageConverter.py
+++ Image-Converter/ImageConverter.py
@@ -1,22 +1,22 @@
 from PIL import Image
 import io
 
-inputFile = "Example Files\CoelaToot.png"
-outputFile = "Output\CoelaToot.h"
-pictureOutput = "Output\CoelaToot.png"
-name = "CoelaToot"
-numColors = 16 
+inputFile = "Example Files\Coela.png"
+outputFolder = "Output\\"
+pictureOutput = "Output\Coela.png"
+name = "Coela"
+numColors = 255 
 
 def GetData(className, file):
-    image = Image.open(file).convert("P", palette = Image.ADAPTIVE, colors = numColors)
+    image = Image.open(file).convert("P", palette=Image.ADAPTIVE, colors=numColors)
     image.save(pictureOutput)
     w, h = image.size
-    
+
     image.seek(0)
     pal = image.getpalette()
 
     data = "#pragma once\n\n"
-    data += "#include \"..\..\Materials\Image.h\"\n\n"
+    data += "#include \"../../../Scene/Materials/Static/Image.h\"\n\n"
     data += "class " + className + " : public Image{\n"
     data += "private:\n"
     data += "\tstatic const uint8_t rgbMemory[];\n"
@@ -27,41 +27,36 @@ def GetData(className, file):
     data += "\t\tSetPosition(offset);\n"
     data += "\t}\n};\n\n"
 
-    data += "const uint8_t " + className + "::rgbMemory[] PROGMEM = {"
-    
+    data2 = "#include \"" + className + ".h\"\n\n"
+
+    data2 += "const uint8_t " + className + "::rgbMemory[] PROGMEM = {"
     for i in range(h):
         for j in range(w):
             index = image.getpixel((j, i))
-
-            data += str(index)
-            
+            data2 += str(index)
             if i == h - 1 and j == w - 1:
-                data += "};\n\n"
+                data2 += "};\n\n"
             else:
-                data += ","
+                data2 += ","
 
-    data += "const uint8_t " + className + "::rgbColors[] PROGMEM = {"
-    
+    data2 += "const uint8_t " + className + "::rgbColors[] PROGMEM = {"
     pal = image.getpalette()
-
     for i in range(numColors):
         r = pal[i * 3]
         g = pal[i * 3 + 1]
         b = pal[i * 3 + 2]
-
-        data += str(r) + "," + str(g) + "," + str(b)
-        
+        data2 += str(r) + "," + str(g) + "," + str(b)
         if i == numColors - 1:
-            data += "};\n"
+            data2 += "};\n"
         else:
-            data += ","
+            data2 += ","
 
-    return data
+    return data, data2
 
+header_data, cpp_data = GetData(name, inputFile)
 
-output = GetData(name, inputFile)
+with open(outputFolder + name + ".h", "w") as f:
+    f.write(header_data)
 
-print(output)
-f = open(outputFile, "w")
-f.write(output)
-f.close()
+with open(outputFolder + name +".cpp", "w") as f:
+    f.write(cpp_data)
